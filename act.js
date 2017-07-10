ad94c9f42e35a8abcf335fe8936ff94199ce3517
Stefan Fotev <sfo321@mail.bg>
2017-07-10 21:20:20 +0300

    act.js and apple.js modified

--- act.js
+++ act.js
@@ -1,42 +1,45 @@
 module.exports = function(db) {
     return {
         setValidation(Model, validator) {
-            const collection = Model.name.toLowerCase() + 's';
-            const validationOptions = {
-                validator: {
-                    $and: [],
-                },
-                validationLevel: 'strict',
-                validationAction: 'error',
-            };
-            const options = {};
-            const model = new Model(options);
-            Object.keys(model)
-                .forEach((prop) => {
-                    if (validator[prop]) {
-                        validationOptions.validator.$and.push({
-                            [prop]: validator[prop],
-                        });
-                        console.log(`Databse validation ${prop} for collection ${collection} set!`);
-                    }
-                });
-
+            const validationOptions = getValidationOptions(Model, validator);
             return db.command({
-                collMod: collection,
-                validator: validationOptions.validator,
+                collMod: validationOptions.collection,
+                validator: validationOptions.v.validator,
+                validationLevel: validationOptions.v.validationLevel,
+                validationAction: validationOptions.v.validationAction,
             });
         },
-        // createCollection(name) {
-        //     return db.createCollection(name, {
-        //         validator: {
-        //             $and: [
-        //                 { phone: { $type: 'string' } },
-        //                 { email: { $regex: /@mongodb\.com$/ } },
-        //                 { status: { $in: ['Unknown', 'Incomplete'] } },
-        //             ],
-        //         },
-        //         validationAction: 'error',
-        //     });
-        // },
+        createCollection(Model, validator) {
+            const validationOptions = getValidationOptions(Model, validator);
+            return db.createCollection(
+                validationOptions.collection,
+                validationOptions.v);
+        },
+    };
+};
+
+const getValidationOptions = (Model, validator) => {
+    const collection = Model.name.toLowerCase() + 's';
+    const validationOptions = {
+        validator: {
+            $and: [{ phone: { $type: 'string' } }],
+        },
+        validationLevel: 'strict',
+        validationAction: 'error',
+    };
+    const options = {};
+    const model = new Model(options);
+    Object.keys(model)
+        .forEach((prop) => {
+            if (validator[prop]) {
+                validationOptions.validator.$and.push({
+                    [prop]: validator[prop],
+                });
+                console.log(`Databse validation ${prop} for collection ${collection} set!`);
+            }
+        });
+    return {
+        collection,
+        v: validationOptions,
     };
 };
--- apple.js
+++ apple.js
@@ -3,14 +3,60 @@ const config = require('./config');
 const Hotel = require('./models/hotel.model');
 const validator = require('./validator');
 
+const fs = require('fs');
+const path = require('path');
+
+// async().then(() => require('./db').setup(config.connectionString)
+//     .then((db) => {
+//         const act = require('./act')(db);
+//         act.setValidation(Hotel, validator)
+//             .then(() => db.close())
+//             .catch((err) => {
+//                 console.log(err);
+//                 db.close();
+//             });
+//     })
+// );
+
 async().then(() => require('./db').setup(config.connectionString)
     .then((db) => {
-        const act = require('./act')(db);
-        act.setValidation(Hotel, validator)
-            .then(() => db.close())
-            .catch((err) => {
-                console.log(err);
-                db.close();
-            });
+        initData(db);
+        // const act = require('./act')(db);
+        // act.setValidation(Hotel, validator)
+        //     .then(() => db.close())
+        //     .catch((err) => {
+        //         console.log(err);
+        //         db.close();
+        //     });
     })
 );
+
+/* globals __dirname */
+
+
+const initData = (db) => {
+    const act = require('./act')(db);
+    const collections = [];
+    fs.readdirSync(__dirname + '/models')
+        .filter((file) => file.includes('.model'))
+        .forEach((file) => {
+            const collection = file.substr(0, file.indexOf('.js'));
+            db.listCollections({ name: collection.substr(0, collection.indexOf('.model')) + 's' })
+                .next(function(err, collinfo) {
+                    if (collinfo) {
+                        const model = require(__dirname + '/models/' + collection);
+
+                        act.setValidation(model, validator);
+                    } else {
+                        const model = require(__dirname + '/models/' + collection);
+                        act.createCollection(model, validator);
+                    }
+                });
+        });
+};
+
+
+// const checkCollection = (db, collection name) => {
+
+// }
+// module.exports = { initData };
