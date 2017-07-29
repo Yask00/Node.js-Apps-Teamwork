9251a6f051203bc7ae66e0e11bed5bf452ab78e1
Yask00 <jasstreb@abv.bg>
2017-07-29 20:27:56 +0300

    Unit, integration and selenium testing preparation + istanbul setup rerun npm install for new dependencies

---
+++ tests/unit/controllers/hotels.controller.js
@@ -0,0 +1,40 @@
+// NOT WORKIG 
+// MISSING USER IN req-res.js
+
+
+// const { expect } = require('chai');
+
+// const { init } = require('../../../app/controllers/hotels.controller');
+
+// describe('Hotels controller', () => {
+//     let data = null;
+//     let controller = null;
+//     const hotels = [1, 2, 3, 4];
+
+//     let req = null;
+//     let res = null;
+
+//     beforeEach(() => {
+//         data = {
+//             hotels: {
+//                 getAll() {
+//                     return Promise.resolve(hotels);
+//                 },
+//             },
+//         };
+
+//         controller = init(data);
+//         req = require('../../unit/req-res').getRequestMock();
+//         res = require('../../unit/req-res').getResponseMock();
+//     });
+
+//     it('expect get all to return all hotels', () => {
+//         return controller.getAll(req, res)
+//             .then(() => {
+//                 expect(res.context).to.be.deep.equal({
+//                     context: hotels,
+//                 });
+//                 expect(res.viewName).to.be.equal('hotel/all');
+//             });
+//     });
+// });
